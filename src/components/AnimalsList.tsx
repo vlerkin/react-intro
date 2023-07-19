255f2ed1c2ed86523025aee35b900a96b7c37306
Valeriia Klestova <valeriya.apukhtina@gmail.com>
2023-07-19 15:57:40 +0200

    Using API to fetch an array of animals in AnimalsList

--- src/components/AnimalsList.tsx
+++ src/components/AnimalsList.tsx
@@ -1,4 +1,7 @@
-import animals from '../data/animals.json';
+//import animals from '../data/animals.json';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Animal } from '../pages/animals';
 
 interface animalKind {
     kind: "cow" | "chicken" | "pig" | "sheep";
@@ -7,6 +10,18 @@ interface animalKind {
 export const animalEmojies = { "cow":"🐮", "chicken":"🐔", "pig":"🐷", "sheep":"🐑"}
 
 const AnimalsList = (props: animalKind) => {
+    const [animals, setAnimals] = useState<Animal[]>([]);
+    useEffect(() => {
+        const getAnimalsFromApi = async () => {
+          const animals = await axios.get(
+            "https://reader.mindmingle.nl/api/exercises/react/animals"
+          );
+          setAnimals(animals.data);
+        };
+     
+        getAnimalsFromApi();
+      }, []);
+    
     return (<>{animals
     .filter((animal) => animal.kind === props.kind)
     .map((animal) => <p>{animalEmojies[props.kind]} {animal.name}</p>)}
--- src/pages/animals/[id].tsx
+++ src/pages/animals/[id].tsx
@@ -3,9 +3,10 @@ import { useRouter } from "next/router";
 import animalsJson from '../../data/animals.json';
 import { Animal } from '../animals';
 import { animalEmojies } from '../../components/AnimalsList';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const pageOfAnAnimal = () => {
+
     
     const animals = animalsJson as Animal[]
     const [animalState, setAnimalState] = useState(animals);
@@ -14,31 +15,40 @@ const pageOfAnAnimal = () => {
     const router = useRouter();
     const idFromUrl = Number(router.query.id);
     const animalFromUrl = animalState.find((animal) => animal.id === idFromUrl);
+
+
+    // /*
+    // const clickHadlerIncrease = () => {
+    //     setCount(count + 1);
+    //     window.alert("You've clicked me and INCREASED the counter!")
+    // };
+    // const clickHadlerDecrease = () => {
+    //     setCount(count - 1);
+    //     window.alert("You've clicked me and DECREASED the counter!")
+    // };
+    // */
+    
+    const fedAnimals =() => {
+        const feedAnimal = animalState.map((animal) => {
+            const newAnimal = {...animal, hasBeenFed:true};
+            return newAnimal;
+        })
+        setAnimalState(feedAnimal);
+    };
+
+    useEffect(() => {
+        console.log("🌾 Yummy!");
+    }, [animalState]);
+ 
+    
     if (!animalFromUrl) {
         return <p>No animal</p>
     }
 
-    /*
-    const clickHadlerIncrease = () => {
-        setCount(count + 1);
-        window.alert("You've clicked me and INCREASED the counter!")
-    };
-    const clickHadlerDecrease = () => {
-        setCount(count - 1);
-        window.alert("You've clicked me and DECREASED the counter!")
-    };
-    */
-    
-    const fedAnimals =() => {
-        const feedAnimal = animalState.map((animal) => {
-        const newAnimal = {...animal, hasBeenFed:true};
-        return newAnimal;
-    })
-        setAnimalState(feedAnimal);
-    };
 
     return (
         <div>
+            Test
             <h1>{ animalFromUrl.name} the {animalFromUrl.kind} {animalEmojies[animalFromUrl.kind]}</h1>
             <p>{`I am ${animalFromUrl.age} years old!`}</p>
             <p>{animalFromUrl.hasBeenFed === true? "😊 I am happy and fed": "🌾 Hungry, give me some food!"}</p>
